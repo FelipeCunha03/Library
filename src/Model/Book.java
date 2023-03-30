dbb96c986eb1bb12dcf8df4e900d14c3916107a6
FelipeCunha03 <114655558+FelipeCunha03@users.noreply.github.com>
2023-03-30 11:22:57 +0100

    It was created class book and try read the file books and pass as obj.

---
+++ src/Model/Book.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author felipecunha
+ */
+public class Book {
+    
+    // Atribute of Book.
+    private String id;
+    private String  firstNameAuthor;
+    private String  LastNameAuthor;
+    private String  bookTitle;
+    private String  genero;
+    
+    // Constructor
+
+    public Book(String id, String firstNameAuthor, String LastNameAuthor, String bookTitle, String genero) {
+        this.id = id;
+        this.firstNameAuthor = firstNameAuthor;
+        this.LastNameAuthor = LastNameAuthor;
+        this.bookTitle = bookTitle;
+        this.genero = genero;
+    }
+    
+    
+    // methodos gets and setters 
+
+    public String getId() {
+        return id;
+    }
+
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstNameAuthor() {
+        return firstNameAuthor;
+    }
+
+    public void setFirstNameAuthor(String firstNameAuthor) {
+        this.firstNameAuthor = firstNameAuthor;
+    }
+
+    public String getLastNameAuthor() {
+        return LastNameAuthor;
+    }
+
+    public void setLastNameAuthor(String LastNameAuthor) {
+        this.LastNameAuthor = LastNameAuthor;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
+    
+    // methodos toString  for output the book as String.
+
+    @Override
+    public String toString() {
+        return "Book{" + "id=" + id + ", firstNameAuthor=" + firstNameAuthor + ", LastNameAuthor=" + LastNameAuthor + ", bookTitle=" + bookTitle + ", genero=" + genero + '}';
+    }
+    
+    
+    
+    
+    
+}
---
+++ src/ca1/MainClass.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca1;
+
+import Model.Book;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author felipecunha
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        // TODO code application logic here
+        
+        //We had decide to use  Set TreeSet because will but in orderalphabetic and Set for avoid duplicate values , makes sense for library system.
+        Set<Book> myBookSet = new TreeSet<>();
+        String path = "src/ca1/Books.csv";//path of data It is.
+        
+        
+         BufferedReader br = new  BufferedReader (new FileReader(path));
+         String line = br.readLine();
+         
+         //We had put all processe for get the book from data  for treatement some exeception if will have some erroe ao open the file.  
+         try{
+             
+             // Sta=t read the file books.The Loop will try get line by line still the next line will be NULL.
+             while(line != null ){
+                     
+                      // Created Array of String  for for get  each information from file CSV.
+                      String [] vetBook = line.split(",");
+                      String id = vetBook[0];
+                      String firstName =  vetBook[1];
+                      String lastName =  vetBook[1];
+                      String title =  vetBook[2];
+                      String genero =  vetBook[3];
+                      
+                      Book bookObj = new Book (id,firstName,lastName,title, genero);
+                      
+                      myBookSet.add(bookObj);
+                      
+                      line = br.readLine(); //  read the next line of file csv.
+                      
+                  }
+             
+         }
+         
+         catch (Exception e) {
+             
+                  System.out.println("Error open file");
+
+         }
+         
+         
+         System.out.println("Book: \n" +myBookSet );
+        
+    }
+    
+}
