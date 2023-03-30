350a7acc44777612b3242fa4265f1f2bceb08e56
FelipeCunha03 <114655558+FelipeCunha03@users.noreply.github.com>
2023-03-30 16:20:11 +0100

    It was created the class student

--- src/library/Library.java
+++ src/library/Library.java
@@ -1,72 +1,67 @@
-    /*
+
+/*
      * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
      * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
-     */
+ */
 
+import Model.Book;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
-    import Model.Book;
-    import java.io.BufferedReader;
-    import java.io.FileNotFoundException;
-    import java.io.FileReader;
-    import java.io.IOException;
-    import java.util.HashSet;
-    import java.util.Set;
-    import java.util.TreeSet;
+/**
+ *
+ * @author felipecunha
+ */
+public class Library {
 
     /**
-     *
-     * @author felipecunha
+     * @param args the command line arguments
      */
-    public class Library {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        // TODO code application logic here
 
-        /**
-         * @param args the command line arguments
-         */
-        public static void main(String[] args) throws FileNotFoundException, IOException {
-            // TODO code application logic here
+        //We had decide to use  Set TreeSet because will but in orderalphabetic and Set for avoid duplicate values , makes sense for library system.
+        Set<Book> myBookSet = new HashSet<>();
+        String path = "src/library/books.csv"; //path of data It is.
 
-            //We had decide to use  Set TreeSet because will but in orderalphabetic and Set for avoid duplicate values , makes sense for library system.
-            Set<Book> myBookSet = new HashSet<>();
-            String path = "src/library/books.csv"; //path of data It is.
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        String line = br.readLine();
 
+        //We had put all processe for get the book from data  for treatement some exeception if will have some erroe ao open the file.  
+        try {
 
+            // Start read the file books.The Loop will try get line by line still the next line will be NULL.
+            while (line != null) {
 
-             BufferedReader br = new  BufferedReader (new FileReader(path));
-             String line = br.readLine();
+                // Created Array of String   for get  each information from file CSV.
+                String[] vetBook = line.split(",");
+                String id = vetBook[0];
+                String firstName = vetBook[1];
+                String lastName = vetBook[2];
+                String title = vetBook[3];
+                String genero = vetBook[4];
 
-             //We had put all processe for get the book from data  for treatement some exeception if will have some erroe ao open the file.  
-             try{
+                Book bookObj = new Book(id, firstName, lastName, title, genero);
 
-                 // Start read the file books.The Loop will try get line by line still the next line will be NULL.
-                 while(line != null ){
+                myBookSet.add(bookObj);
 
-                          // Created Array of String   for get  each information from file CSV.
-                          String [] vetBook = line.split(",");
-                          String id = vetBook[0];
-                          String firstName =  vetBook[1];
-                          String lastName =  vetBook[2];
-                          String title =  vetBook[3];
-                          String genero =  vetBook[4];
+                line = br.readLine(); //  read the next line of file csv.
 
-                          Book bookObj = new Book (id,firstName,lastName,title,genero);
+            }
 
-                          myBookSet.add(bookObj);
+        } catch (Exception e) {
 
-                          line = br.readLine(); //  read the next line of file csv.
-
-                      }
-
-             }
-
-             catch (Exception e) {
-
-                      System.out.println("Error open file" + e.getMessage());
-
-             }
-
-
-             System.out.println(myBookSet);
+            System.out.println("Error open file" + e.getMessage());
 
         }
 
+        System.out.println(myBookSet);
+
     }
+
+}
