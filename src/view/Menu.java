e85ee0a542e63b6de077c5fe02ea0b361d371004
FelipeCunha03 <114655558+FelipeCunha03@users.noreply.github.com>
2023-04-03 23:47:05 +0100

    It was added try for treatment exceptions if the user enter something different than number and added default on the swicwt case if the user pick up another wrong option.

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -14,23 +14,22 @@ import java.util.Scanner;
  * @author rapha
  */
 public class Menu {
-    
-    public static void main(String[] args){
-    
+
+    public static void main(String[] args) {
+
         boolean valid;
         int option;
-        
-        Scanner myScanner = new Scanner (System.in);
+
+        Scanner myScanner = new Scanner(System.in);
         cotrollerBook myCB = new cotrollerBook();
         controllerStudent myCS = new controllerStudent();
         controllerBorrowing myBw = new controllerBorrowing();
         String bookTitle, bookAuthor;
-   
-        
-        do{
-            
+
+        do {
+
             valid = false;
-            
+
             System.out.println("**********************************************************");
             System.out.println("**                 Welcome to CCT Library!!             **");
             System.out.println("**********************************************************");
@@ -46,69 +45,74 @@ public class Menu {
             System.out.println("**  8. List all students by ID;                         **");
             System.out.println("**  9. Register a student has borrowed a book;          **");
             System.out.println("**  10. Register a student has returned a book;         **");
-            System.out.println("**  11. List the books borrowed by a specific student   **");         
+            System.out.println("**  11. List the books borrowed by a specific student   **");
             System.out.println("**  12. Exit                                            **");
             System.out.println("**********************************************************");
             System.out.println("**********************************************************");
-               
+
             System.out.println("\n==> Please enter your option: ");
-            
-            option = myScanner.nextInt();
-            
-            switch (option){
+            try {
+                option = myScanner.nextInt();
+
+                switch (option) {
+
+                    case (1):
+                        System.out.println("Inform the book title: ");
+                        bookTitle = myScanner.nextLine().trim();
+
+                        myCB.searchBookTitle(bookTitle);
+                        break;
+
+                    case (2):
+                        System.out.println("Inform the author's name: ");
+                        bookAuthor = myScanner.nextLine().trim();
+
+                        myCB.searchBookAuthor();
+                        break;
+
+                    case (3):
+                        myCB.listBookTitle();
+                        break;
+                    case (4):
+                        myCB.listBookAuthor();
+                        break;
+                    case (5):
+                        myCS.searchStudentName();
+                        break;
+                    case (6):
+                        myCS.searchStudentID();
+                        break;
+                    case (7):
+                        myCS.listStudentName();
+                        break;
+                    case (8):
+                        myCS.listStudentID();
+                        break;
+                    case (9):
+                        myBw.borrowBook();
+                        break;
+                    case (10):
+                        myBw.returnBook();
+                        break;
+                    case (11):
+                        myBw.listBookBorrowed();
+                        break;
+                    case (12):
+                        System.out.println("-The program end.-");
+                        valid = true;
+                        break;
+
+                    default:
+                        System.out.println("Wrong option! choos option between 1 to 12");
+
+                }
+            } catch (Exception e) {
+                System.out.println("ERROR! JUSTE NUMBER!");
+                myScanner.nextLine();
                 
-                case (1):
-                    System.out.println("Inform the book title: ");  
-                    bookTitle = myScanner.nextLine().trim();
-                    
-                    myCB.searchBookTitle(bookTitle);
-                    break;
-                    
-                case (2):
-                    System.out.println("Inform the author's name: ");  
-                    bookAuthor = myScanner.nextLine().trim();
-                    
-                    myCB.searchBookAuthor();
-                    break;
-                    
-                case (3):
-                    myCB.listBookTitle();
-                    break;
-                case (4):
-                    myCB.listBookAuthor();
-                    break;
-                case (5):
-                    myCS.searchStudentName();
-                    break;
-                case (6):
-                    myCS.searchStudentID();
-                    break;
-                case (7):
-                    myCS.listStudentName();
-                    break;
-                case (8):
-                    myCS.listStudentID();
-                    break;
-                case (9):
-                    myBw.borrowBook();
-                    break;
-                case (10):
-                    myBw.returnBook();
-                    break;
-                case (11):
-                    myBw.listBookBorrowed();
-                    break;
-                case (12): 
-                    System.out.println("-The program end.-");                        
-                    break;  
-                               
             }
-            if (option < 1 || option > 12){ 
-                System.out.println("Type a valid option please.");
-            }
-            valid = true;
-        }while(!valid);
+
+        } while (!valid);
     }
-    
 
 }
